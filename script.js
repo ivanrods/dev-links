6c0e0aa3f61db18fa554af4e93e1d52e2e0279bb
ivanrods <contaivanrodrigues@gmail.com>
2023-08-01 10:46:44 -0300

    finalizando devlinks

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function toggleMode(){
+function toggleMode() {
     const html = document.documentElement
-    const img = document.querySelector('#profile img')
-    if(html.classList.contains('light')){
-        html.classList.remove('light')
-        img.setAttribute('src', 'assets/avatar-light.png')
-    }else{
-        html.classList.add('light')
-        img.setAttribute('src', 'assets/avatar.png')
+    html.classList.toggle("light")
+    const img = document.querySelector("#profile img")
+  
+    if (html.classList.contains("light")) {
+      img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+      img.setAttribute("src", "./assets/avatar.png")
     }
-}
+  }
